35d8b4a11a4de4b0fa096156ad410b1c6e53a0a4
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2024-11-13 15:26:42 +0100

    feat(model): Implemented the basic Human class

--- src/com/biocollapse/model/Human.java
+++ src/com/biocollapse/model/Human.java
@@ -1,5 +1,162 @@
 package src.com.biocollapse.model;
 
+import src.com.biocollapse.util.GlobalRandom;
+
+enum GoalState {
+  to_home,
+  to_work,
+  to_hospital,
+}
+
 public class Human {
-  
+  private boolean infected;
+  private int infectedAt;
+  private boolean alive = true;
+  private boolean immune;
+  private MapPosition pos;
+  private MapPosition goalPos;
+  private GoalState goalState = GoalState.to_home;
+  private MapPosition workPos;
+  private MapPosition homePos;
+  // ! I dont know if its a good idea to store the config inside of every human
+  // ! We should change that later on! I just don't want to think about it right now.
+  private Config config;
+
+  Human(Config config, boolean infected, boolean immune, MapPosition pos, MapPosition workPos, MapPosition homePos) {
+    this.config = config;
+    this.infected = infected;
+    this.immune = immune;
+    this.pos = pos;
+    this.workPos = workPos;
+    this.homePos = homePos;
+
+    // On init a human spwans at an unknown
+    // location and his first action will be to walk home.
+    this.goalPos = homePos;
+  }
+
+  public boolean isInfected() {
+    return infected;
+  }
+
+  public void setInfected(boolean infected) {
+    this.infected = infected;
+  }
+
+  public int getInfectedAt() {
+    return infectedAt;
+  }
+
+  public void setInfectedAt(int infectedAt) {
+    this.infectedAt = infectedAt;
+  }
+
+  public boolean isAlive() {
+    return alive;
+  }
+
+  public void setAlive(boolean alive) {
+    this.alive = alive;
+  }
+
+  public boolean isImmune() {
+    return immune;
+  }
+
+  public void setImmune(boolean immune) {
+    this.immune = immune;
+  }
+
+  public MapPosition getPos() {
+    return pos;
+  }
+
+  public void setPos(MapPosition pos) {
+    this.pos = pos;
+  }
+
+  public MapPosition getGoalPos() {
+    return goalPos;
+  }
+
+  public void setGoalPos(MapPosition goalPos) {
+    this.goalPos = goalPos;
+  }
+
+  public GoalState getGoalState() {
+    return goalState;
+  }
+
+  public void setGoalState(GoalState goalState) {
+    this.goalState = goalState;
+  }
+
+  public MapPosition getWorkPos() {
+    return workPos;
+  }
+
+  public void setWorkPos(MapPosition workPos) {
+    this.workPos = workPos;
+  }
+
+  public MapPosition getHomePos() {
+    return homePos;
+  }
+
+  public void setHomePos(MapPosition homePos) {
+    this.homePos = homePos;
+  }
+
+  public Config getConfig() {
+    return config;
+  }
+
+  public void setConfig(Config config) {
+    this.config = config;
+  }
+
+  // A person is always travelling back and forth between home and work.
+  // If the person is infected, there is a probability that the person will go to hospital. 
+  // If the person arrives at home, there is a probability that
+  // he can continue to stay at home instead of going to work.
+  public void updateHumanGoal() {
+    if (infected) {
+      // If the person is infected, they have a chance to change their destination to a hospital.
+      if (goalState != GoalState.to_hospital && GlobalRandom.checkProbability(config.getHospitalProbability())) {
+        goalState = GoalState.to_hospital;
+        // TODO Implement a function to get a Hospital position
+        goalPos = new MapPosition(0, 0);
+      }
+    } else {
+      // When a person reaches their destination, they return home or go to work.
+      if (pos.equals(goalPos)) {
+        if (goalState == GoalState.to_work) {
+          goalState = GoalState.to_home;
+          goalPos = homePos;
+        } else {
+          // If the person arrived at home there is a chance that the person will stay at home
+          if (!GlobalRandom.checkProbability(config.getIsolationProbability())) {
+            goalState = GoalState.to_work;
+            goalPos = workPos;
+          }
+        }
+      }
+    }
+  }
+
+
+  @Override
+  public String toString() {
+    return "Human{" +
+        "infected=" + infected +
+        ", infectedAt=" + infectedAt +
+        ", alive=" + alive +
+        ", immune=" + immune +
+        ", pos=" + (pos != null ? pos.toString() : "null") +
+        ", goalPos=" + (goalPos != null ? goalPos.toString() : "null") +
+        ", goalState=" + goalState.toString() +
+        ", workPos=" + (workPos != null ? workPos.toString() : "null") +
+        ", homePos=" + (homePos != null ? homePos.toString() : "null") +
+        '}';
+  }
 }
