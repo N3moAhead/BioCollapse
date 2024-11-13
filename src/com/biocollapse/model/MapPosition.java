8221988ae26da909bcb353d1c918ecc6fa7d881a
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2024-11-13 12:59:12 +0100

    feat(model): Extended the MapPosition class

--- src/com/biocollapse/model/MapPosition.java
+++ src/com/biocollapse/model/MapPosition.java
@@ -1,5 +1,7 @@
 package src.com.biocollapse.model;
 
+import java.util.Map;
+
 public class MapPosition {
   private int row;
   private int col;
@@ -33,6 +35,46 @@ public class MapPosition {
   public void subtract(MapPosition pos) {
     this.row -= pos.row;
     this.col -= pos.col;
+  }
+
+  // Move position up one field 
+  public void moveUp() {
+    this.row -= 1;
+  }
+
+  // Move position one field to the right
+  public void moveRight() {
+    this.col += 1;
+  }
+
+  // Move position down one field
+  public void moveDown() {
+    this.row += 1;
+  }
+
+  // Move position one field to the left
+  public void moveLeft() {
+    this.col -= 1;
+  }
+
+  // Returns the position above the current position
+  public MapPosition getTop() {
+    return new MapPosition(row - 1, col);
+  }
+
+  // Returns the position to the right of the current position
+  public MapPosition getRight() {
+    return new MapPosition(row, col + 1);
+  }
+
+  // Returns the position below the current position
+  public MapPosition getBot() {
+    return new MapPosition(row + 1, col);
+  }
+
+  // Returns the position to the left of the current position
+  public MapPosition getLeft() {
+    return new MapPosition(row, col - 1);
   }
 
   @Override
