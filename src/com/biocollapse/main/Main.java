f1a09c56e391d7ff94bb4e094374fc75da9cb799
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2024-11-28 12:41:58 +0100

    fix(Main): Removed useless import

--- src/com/biocollapse/main/Main.java
+++ src/com/biocollapse/main/Main.java
@@ -1,7 +1,6 @@
 package src.com.biocollapse.main;
 
 import src.com.biocollapse.controller.WindowController;
-import src.com.biocollapse.model.Map;
 
 public class Main {
   public static void main(String[] args) {
