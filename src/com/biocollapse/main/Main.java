121437d6b068080b1db53a4d1b3e8501200c9e37
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2024-11-28 12:41:19 +0100

    fix(Main): Removed debugging code

--- src/com/biocollapse/main/Main.java
+++ src/com/biocollapse/main/Main.java
@@ -6,11 +6,6 @@ import src.com.biocollapse.model.Map;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Welcome to BioCollapse");
-    Map map = new Map();
-    map.printMap();
-
-    // Testing SplashScreen
-    
     WindowController controller = new WindowController();
     controller.showSplashScreen();
   }
