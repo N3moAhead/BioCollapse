653bd19afa8e7d692d76af2bdfdc51a1d3fd99ac
joewilliams007 <williamsthehack@gmail.com>
2024-12-04 14:35:14 +0100

    The legend now has a background and box.

--- src/com/biocollapse/view/GraphPanel.java
+++ src/com/biocollapse/view/GraphPanel.java
@@ -12,6 +12,7 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.util.Map;
 import java.util.Map.Entry;
 import javax.swing.JPanel;
@@ -24,10 +25,14 @@ public class GraphPanel extends JPanel {
     private int height;
     private double cellSize;
     private int highestVisibleNum;
+    private int legendY;
 
     private int visiblePoints = 50;
     private boolean drawPoints = true;
     private boolean drawConnections = true;
+
+    private static final String TEXT_AMOUNT = "Anzahl";
+    private static final String TEXT_TIME = "Zeit";
 
     private static final int PADDING_TOP = 20;
     private static final int PADDING_BOTTOM = 20;
@@ -42,7 +47,7 @@ public class GraphPanel extends JPanel {
 
         width = getWidth();
         height = getHeight();
-        setVisiblePoints(visiblePoints);
+        cellSize = (width - PADDING_LEFT - PADDING_RIGHT) / visiblePoints;
         addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
@@ -77,7 +82,7 @@ public class GraphPanel extends JPanel {
     }
 
     /**
-     * Wheter to draw or not draw connections between points. Setting to false will activate points.
+     * Whether to draw or not draw connections between points. Setting to false will activate points.
      * @param drawConnections
      */
     public void setDrawConnections(boolean drawConnections) {
@@ -88,14 +93,23 @@ public class GraphPanel extends JPanel {
         }
     }
     
+    /**
+     * @return the max amount of points that should be drawn.
+     */
     public int getVisiblePoints() {
         return visiblePoints;
     }
 
+    /**
+     * @return whether or not each point should have it's own circle drawn.
+     */
     public boolean isDrawPoints() {
         return drawPoints;
     }
 
+    /**
+     * @return whether or not the connections from point to point should be drawn.
+     */
     public boolean isDrawConnections() {
         return drawConnections;
     }
@@ -104,35 +118,44 @@ public class GraphPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         highestVisibleNum = getHighestVisibleNum();
-        legendY = 0;
+        legendY = 10;
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
+        // Draw the layout details.
         g.setColor(Color.BLACK);
         g.setFont(new Font("Arial", Font.PLAIN, 10));
-        g.drawString("amount", 4, 10);
-        g.drawString("time", width - 20 - PADDING_RIGHT, height - 7);
+        g.drawString(TEXT_AMOUNT, 4, 10);
+        g.drawString(TEXT_TIME, width - 20 - PADDING_RIGHT, height - 7);
         g.drawString("0", 2, height - 7);
 
+        // Draws the value indications for each quater.
         drawLeftString(String.valueOf(highestVisibleNum), g2d, 2, PADDING_TOP + 5);
         drawLeftString(String.valueOf((float) highestVisibleNum / 2), g2d, 2, height / 2 + 5);
         drawLeftString(String.valueOf((float) highestVisibleNum / 4 + highestVisibleNum / 2), g2d, 2, height / 4 + 5);
         drawLeftString(String.valueOf((float) highestVisibleNum / 4), g2d, 2, height / 4 * 3 + 5);
 
-        g2d.drawLine(PADDING_LEFT, height - PADDING_BOTTOM, width - PADDING_RIGHT, height - PADDING_BOTTOM); // X-axis
-        g2d.drawLine(PADDING_LEFT, height - PADDING_BOTTOM, PADDING_LEFT, PADDING_BOTTOM);                 // Y-axis
+        // Draw X and Y lines.
+        g2d.drawLine(PADDING_LEFT, height - PADDING_BOTTOM, width - PADDING_RIGHT, height - PADDING_BOTTOM);
+        g2d.drawLine(PADDING_LEFT, height - PADDING_BOTTOM, PADDING_LEFT, PADDING_BOTTOM);
 
+        // Draw the points.
         for (Entry<String, Graph> entry : graphs.entrySet()) {
             drawPoints(entry.getValue(), g2d);
         }
 
-        drawLegend(g2d);
+        int legendSize = getLongestLegendLength(g2d);
+        drawLegendBackground(g2d, legendSize);
+        drawLegend(g2d, legendSize);
     }
 
-    private void drawLegend(Graphics2D g2d) {
-        int legendSize = getLongestLegendLength(g2d);
-        g2d.setFont(new Font("Arial", Font.PLAIN, 8));
+    /**
+     * Draws the legend for the graphs.
+     * @param g2d
+     */
+    private void drawLegend(Graphics2D g2d, int legendSize) {
+        g2d.setFont(new Font("Arial", Font.PLAIN, LEGEND_FONT_SIZE));
         for (Entry<String, Graph> entry : graphs.entrySet()) {
             if (entry.getValue().isAlwaysHidden() || !entry.getValue().isVisible()) {
                 return;
@@ -140,23 +163,37 @@ public class GraphPanel extends JPanel {
             drawLegendString(entry.getKey(), entry.getValue().getColor(), g2d, legendSize);
         }
     }
-
-
-    private int legendY = 0;
     
+    private static final int LEGEND_FONT_SIZE = 8;
+    private static final int LEGEND_MARGIN = 10;
+
     /**
-     * Draw legend String.
+     * Draw a legends text.
      */
     private void drawLegendString(String text, Color c, Graphics2D g2d, int legendSize) {
         g2d.setColor(c);
-        int lineStart = width - legendSize - 20;
+        int lineStart = width - legendSize - LEGEND_MARGIN * 2;
         int lineEnd = width - legendSize;
 
-        g2d.drawLine(lineStart, 10 + legendY, lineEnd, 10 + legendY);
+        g2d.drawLine(lineStart, LEGEND_MARGIN + legendY, lineEnd, LEGEND_MARGIN + legendY);
         legendY += g2d.getFontMetrics().getHeight();
 
         g2d.setColor(Color.BLACK);
         g2d.drawString(text, lineEnd+5, legendY+4);
+    }
+
+    /**
+     * Draw the legend background.
+     */
+    private void drawLegendBackground(Graphics2D g2d, int legendSize) {
+        g2d.setColor(Color.WHITE);
+        int startX = width - legendSize -30;
+        int endY = graphs.size()*LEGEND_FONT_SIZE+5;
+        g2d.fill(new Rectangle2D.Double(startX, 10, legendSize +LEGEND_MARGIN * 2, endY));
+
+        g2d.setColor(Color.BLACK);
+        g2d.drawRect(startX, 10, legendSize+LEGEND_MARGIN * 2, endY);
+        setMinimumSize(new Dimension(10, (endY+2*LEGEND_MARGIN)*2));
     }
 
     /**
@@ -202,19 +239,20 @@ public class GraphPanel extends JPanel {
     }
 
     /**
-     * Draw the points of a graph.
+     * Draw all points of a given graph.
      */
     private void drawPoints(Graph graph, Graphics2D g2d) {
         if (graph.isAlwaysHidden() || !graph.isVisible()) {
             return;
         }
+        g2d.setColor(graph.getColor());
+        
         double prevX = PADDING_LEFT;
         double prevY = height - PADDING_BOTTOM;
 
-        g2d.setColor(graph.getColor());
-
         int i = 0;
         int size = 0;
+        
         for (int j = graph.size()-1; j >= 0; j--) {
             size+=cellSize;
             if (size > getWidth()) {
@@ -228,13 +266,11 @@ public class GraphPanel extends JPanel {
                 oval.setFrame(x - 3, y - 3, 6, 6);
                 g2d.fill(oval);
             }
-
-            if (i != 0 && drawConnections) {
-                g2d.draw(new Line2D.Double(prevX, prevY, x, y)); // Vertical lines
+            if (drawConnections && i != 0) {
+                g2d.draw(new Line2D.Double(prevX, prevY, x, y));
             }
             prevX = x;
             prevY = y;
-
             i++;
         }
     }
