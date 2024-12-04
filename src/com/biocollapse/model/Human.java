2c298601d4707650b4601e132b2b68375d511743
InaasHammoush <121546232+InaasHammoush@users.noreply.github.com>
2024-12-04 12:23:15 +0100

    fixed indentation

--- src/com/biocollapse/model/Human.java
+++ src/com/biocollapse/model/Human.java
@@ -5,250 +5,250 @@ import src.com.biocollapse.util.GlobalConfig;
 import src.com.biocollapse.util.GlobalRandom;
 
 public class Human {
-  private Age age;
-  private boolean infected;
-  private int infectedAt;
-  private boolean hospitalized = false;
-  private boolean alive = true;
-  private boolean immune;
-  private MapPosition pos;
-  private MapPosition goalPos;
-  private MapPosition workPos;
-  private MapPosition homePos;
-  private MapPosition previouPosition;
-  private Integer reachedLocationAt = null;
+	private Age age;
+	private boolean infected;
+	private int infectedAt;
+	private boolean hospitalized = false;
+	private boolean alive = true;
+	private boolean immune;
+	private MapPosition pos;
+	private MapPosition goalPos;
+	private MapPosition workPos;
+	private MapPosition homePos;
+	private MapPosition previouPosition;
+	private Integer reachedLocationAt = null;
 
-  public MapPosition getPreviouPosition() {
-    return previouPosition;
-  }
+	public MapPosition getPreviouPosition() {
+		return previouPosition;
+	}
 
-  public void setPreviouPosition(MapPosition previouPosition) {
-    this.previouPosition = previouPosition;
-  }
+	public void setPreviouPosition(MapPosition previouPosition) {
+		this.previouPosition = previouPosition;
+	}
 
-  public Human(boolean infected, boolean immune, MapPosition pos, MapPosition workPos, MapPosition homePos) {
-    this.infected = infected;
-    this.immune = immune;
-    this.pos = pos;
-    this.workPos = workPos;
-    this.homePos = homePos;
+	public Human(boolean infected, boolean immune, MapPosition pos, MapPosition workPos, MapPosition homePos) {
+		this.infected = infected;
+		this.immune = immune;
+		this.pos = pos;
+		this.workPos = workPos;
+		this.homePos = homePos;
 
-    // On init a human spwans at an unknown
-    // location and his first action will be to walk home.
-    this.goalPos = homePos;
-  }
+		// On init a human spwans at an unknown
+		// location and his first action will be to walk home.
+		this.goalPos = homePos;
+	}
 
-  /**
-   * updates the destination of the human according to different probabilites
-   * 
-   * @param map  the map where a human walks
-   * @param tick current tick
-   */
-  public void updateHumanGoal(Map map, int tick) {
-    Block blockGoal = map.getBlock(this.goalPos);
-    if (this.infected) {
-      int hospitalProbability = GlobalConfig.config.getHospitalProbability();
-      int isolationProbability = GlobalConfig.config.getIsolationProbability();
-      // If the person is infected, they have a chance to change their destination to
-      // a hospital or stay at home.
-      if (GlobalConfig.config.getIsolationMandate()) {
-        isolationProbability = isolationProbability * GlobalConfig.config.getIsolationEffect();
-      }
-      // If human is not on its way to a hospital and the hospitalProbability checks
-      // the human will go to the hospital
-      if (blockGoal != Block.Hospital && GlobalRandom.checkProbability(hospitalProbability)) {
-        MapPosition nearestHospital = map.findNearest(Block.Hospital, this.pos.copy());
-        if (nearestHospital != null) {
-          this.setGoalPos(nearestHospital);
-        }
-      } else if (blockGoal != Block.Hospital && GlobalRandom.checkProbability(isolationProbability)) {
-        // Todo ensure that the isolation does not get revoked in the next
-        this.setGoalPos(homePos);
-      }
-    } else {
-      // When a person reaches their destination, they return home or go to work.
-      if (this.pos.equals(this.goalPos)) {
-        // let them stay at home for a while
-        if (this.reachedLocationAt == null) {
-          this.setReachedLocationAt(tick);
-        } else {
-          // If the human has not stayed long enough at one specific location he will stay
-          // there for a while
-          if (tick - reachedLocationAt < GlobalConfig.config.getTicksAtLocation())
-            return;
-          // There is a chance that a person will stay at home which can be increased when
-          // the lockdownMandate is true
-          int lockdownProbability = 0;
-          int effectiveLockdownProbability;
-          if (GlobalConfig.config.getLockdown() || GlobalConfig.config.getSchoolClosure()) {
-            effectiveLockdownProbability = GlobalConfig.config.getLockdownEffect();
-          } else {
-            effectiveLockdownProbability = lockdownProbability;
-          }
-          if (blockGoal == Block.Workplace) {
-            // set it to null because we left the current location
-            this.setReachedLocationAt(null);
-            this.setGoalPos(this.homePos);
-          } else {
-            // There is a chance that a person will stay at home
-            if (GlobalRandom.checkProbability(effectiveLockdownProbability)) {
-              this.setReachedLocationAt(tick);
-            } else {
-              // Set it to null because we left the current location
-              this.setReachedLocationAt(null);
-              this.setGoalPos(workPos);
-            }
-          }
-        }
-      }
-    }
-  }
+	/**
+	 * updates the destination of the human according to different probabilites
+	 * 
+	 * @param map  the map where a human walks
+	 * @param tick current tick
+	 */
+	public void updateHumanGoal(Map map, int tick) {
+		Block blockGoal = map.getBlock(this.goalPos);
+		if (this.infected) {
+			int hospitalProbability = GlobalConfig.config.getHospitalProbability();
+			int isolationProbability = GlobalConfig.config.getIsolationProbability();
+			// If the person is infected, they have a chance to change their destination to
+			// a hospital or stay at home.
+			if (GlobalConfig.config.getIsolationMandate()) {
+				isolationProbability = isolationProbability * GlobalConfig.config.getIsolationEffect();
+			}
+			// If human is not on its way to a hospital and the hospitalProbability checks
+			// the human will go to the hospital
+			if (blockGoal != Block.Hospital && GlobalRandom.checkProbability(hospitalProbability)) {
+				MapPosition nearestHospital = map.findNearest(Block.Hospital, this.pos.copy());
+				if (nearestHospital != null) {
+					this.setGoalPos(nearestHospital);
+				}
+			} else if (blockGoal != Block.Hospital && GlobalRandom.checkProbability(isolationProbability)) {
+				// Todo ensure that the isolation does not get revoked in the next
+				this.setGoalPos(homePos);
+			}
+		} else {
+			// When a person reaches their destination, they return home or go to work.
+			if (this.pos.equals(this.goalPos)) {
+				// let them stay at home for a while
+				if (this.reachedLocationAt == null) {
+					this.setReachedLocationAt(tick);
+				} else {
+					// If the human has not stayed long enough at one specific location he will stay
+					// there for a while
+					if (tick - reachedLocationAt < GlobalConfig.config.getTicksAtLocation())
+						return;
+					// There is a chance that a person will stay at home which can be increased when
+					// the lockdownMandate is true
+					int lockdownProbability = 0;
+					int effectiveLockdownProbability;
+					if (GlobalConfig.config.getLockdown() || GlobalConfig.config.getSchoolClosure()) {
+						effectiveLockdownProbability = GlobalConfig.config.getLockdownEffect();
+					} else {
+						effectiveLockdownProbability = lockdownProbability;
+					}
+					if (blockGoal == Block.Workplace) {
+						// set it to null because we left the current location
+						this.setReachedLocationAt(null);
+						this.setGoalPos(this.homePos);
+					} else {
+						// There is a chance that a person will stay at home
+						if (GlobalRandom.checkProbability(effectiveLockdownProbability)) {
+							this.setReachedLocationAt(tick);
+						} else {
+							// Set it to null because we left the current location
+							this.setReachedLocationAt(null);
+							this.setGoalPos(workPos);
+						}
+					}
+				}
+			}
+		}
+	}
 
-  /**
-   * Moves the human towards the goal position by evaluating the least amount
-   * of steps it would take to walk to the destination.
-   *
-   * @param map the map where a human moves
-   */
-  public void move(Map map) {
-    MovementAction bestDirection = MovementAction.NONE;
-    int smallestSteps = Integer.MAX_VALUE;
+	/**
+	 * Moves the human towards the goal position by evaluating the least amount
+	 * of steps it would take to walk to the destination.
+	 *
+	 * @param map the map where a human moves
+	 */
+	public void move(Map map) {
+		MovementAction bestDirection = MovementAction.NONE;
+		int smallestSteps = Integer.MAX_VALUE;
 
-    if (this.pos.equals(this.goalPos)) {
-      return;
-    }
+		if (this.pos.equals(this.goalPos)) {
+			return;
+		}
 
-    Integer[][] stepMatrix = map.getStepMatrix(this.goalPos, this.pos);
-    if (stepMatrix == null) {
-      return;
-    }
+		Integer[][] stepMatrix = map.getStepMatrix(this.goalPos, this.pos);
+		if (stepMatrix == null) {
+			return;
+		}
 
-    // Check all four directions
-    for (MovementAction direction : MovementAction.values()) {
-      if (direction != MovementAction.NONE){  // Skip "NONE" direction
-        try {
-          MapPosition newPos = this.pos.getByMove(direction);
-          Block blockAtNewPos = map.getBlock(newPos);
-  
-          // Only consider positions that are walkable
-          if (blockAtNewPos == Block.Path || newPos.equals(this.goalPos)) {
-            Integer currentSteps = stepMatrix[newPos.getRow()][newPos.getCol()];
-            if (currentSteps != null && currentSteps < smallestSteps) {
-              smallestSteps = currentSteps;
-              bestDirection = direction;
-            }
-          }
-        } catch (IndexOutOfBoundsException e) {
-          // Ignore positions outside the map
-        }
-      }
-    }
-    moveIntoDirection(bestDirection);
-  }
+		// Check all four directions
+		for (MovementAction direction : MovementAction.values()) {
+			if (direction != MovementAction.NONE) { // Skip "NONE" direction
+				try {
+					MapPosition newPos = this.pos.getByMove(direction);
+					Block blockAtNewPos = map.getBlock(newPos);
 
-  public Integer getReachedLocationAt() {
-    return reachedLocationAt;
-  }
+					// Only consider positions that are walkable
+					if (blockAtNewPos == Block.Path || newPos.equals(this.goalPos)) {
+						Integer currentSteps = stepMatrix[newPos.getRow()][newPos.getCol()];
+						if (currentSteps != null && currentSteps < smallestSteps) {
+							smallestSteps = currentSteps;
+							bestDirection = direction;
+						}
+					}
+				} catch (IndexOutOfBoundsException e) {
+					// Ignore positions outside the map
+				}
+			}
+		}
+		moveIntoDirection(bestDirection);
+	}
 
-  public void setReachedLocationAt(Integer reachedLocationAt) {
-    this.reachedLocationAt = reachedLocationAt;
-  }
+	public Integer getReachedLocationAt() {
+		return reachedLocationAt;
+	}
 
-  public Age getAge() {
-    return this.age;
-  }
+	public void setReachedLocationAt(Integer reachedLocationAt) {
+		this.reachedLocationAt = reachedLocationAt;
+	}
 
-  public void setAge(Age age) {
-    this.age = age;
-  }
+	public Age getAge() {
+		return this.age;
+	}
 
-  public boolean isInfected() {
-    return infected;
-  }
+	public void setAge(Age age) {
+		this.age = age;
+	}
 
-  public void setInfected(boolean infected) {
-    this.infected = infected;
-  }
+	public boolean isInfected() {
+		return infected;
+	}
 
-  public int getInfectedAt() {
-    return infectedAt;
-  }
+	public void setInfected(boolean infected) {
+		this.infected = infected;
+	}
 
-  public void setInfectedAt(int infectedAt) {
-    this.infectedAt = infectedAt;
-  }
+	public int getInfectedAt() {
+		return infectedAt;
+	}
 
-  public boolean isHospitalized() {
-    return hospitalized;
-  }
+	public void setInfectedAt(int infectedAt) {
+		this.infectedAt = infectedAt;
+	}
 
-  public void setHospitalized(boolean hosptialized) {
-    this.hospitalized = hosptialized;
-  }
+	public boolean isHospitalized() {
+		return hospitalized;
+	}
 
-  public boolean isAlive() {
-    return alive;
-  }
+	public void setHospitalized(boolean hosptialized) {
+		this.hospitalized = hosptialized;
+	}
 
-  public void setAlive(boolean alive) {
-    this.alive = alive;
-  }
+	public boolean isAlive() {
+		return alive;
+	}
 
-  public boolean isImmune() {
-    return immune;
-  }
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
 
-  public void setImmune(boolean immune) {
-    this.immune = immune;
-  }
+	public boolean isImmune() {
+		return immune;
+	}
 
-  public MapPosition getPos() {
-    return pos;
-  }
+	public void setImmune(boolean immune) {
+		this.immune = immune;
+	}
 
-  public void setPos(MapPosition pos) {
-    this.pos = pos;
-  }
+	public MapPosition getPos() {
+		return pos;
+	}
 
-  public void moveIntoDirection(MovementAction direction) {
-    this.pos.moveIntoDirection(direction);
-  }
+	public void setPos(MapPosition pos) {
+		this.pos = pos;
+	}
 
-  public MapPosition getGoalPos() {
-    return goalPos;
-  }
+	public void moveIntoDirection(MovementAction direction) {
+		this.pos.moveIntoDirection(direction);
+	}
 
-  public void setGoalPos(MapPosition goalPos) {
-    this.goalPos = goalPos;
-  }
+	public MapPosition getGoalPos() {
+		return goalPos;
+	}
 
-  public MapPosition getWorkPos() {
-    return workPos;
-  }
+	public void setGoalPos(MapPosition goalPos) {
+		this.goalPos = goalPos;
+	}
 
-  public void setWorkPos(MapPosition workPos) {
-    this.workPos = workPos;
-  }
+	public MapPosition getWorkPos() {
+		return workPos;
+	}
 
-  public MapPosition getHomePos() {
-    return homePos;
-  }
+	public void setWorkPos(MapPosition workPos) {
+		this.workPos = workPos;
+	}
 
-  public void setHomePos(MapPosition homePos) {
-    this.homePos = homePos;
-  }
+	public MapPosition getHomePos() {
+		return homePos;
+	}
 
-  @Override
-  public String toString() {
-    return "Human{" +
-        "infected=" + infected +
-        ", infectedAt=" + infectedAt +
-        ", alive=" + alive +
-        ", immune=" + immune +
-        ", pos=" + (pos != null ? pos.toString() : "null") +
-        ", goalPos=" + (goalPos != null ? goalPos.toString() : "null") +
-        ", workPos=" + (workPos != null ? workPos.toString() : "null") +
-        ", homePos=" + (homePos != null ? homePos.toString() : "null") +
-        '}';
-  }
+	public void setHomePos(MapPosition homePos) {
+		this.homePos = homePos;
+	}
+
+	@Override
+	public String toString() {
+		return "Human{" +
+				"infected=" + infected +
+				", infectedAt=" + infectedAt +
+				", alive=" + alive +
+				", immune=" + immune +
+				", pos=" + (pos != null ? pos.toString() : "null") +
+				", goalPos=" + (goalPos != null ? goalPos.toString() : "null") +
+				", workPos=" + (workPos != null ? workPos.toString() : "null") +
+				", homePos=" + (homePos != null ? homePos.toString() : "null") +
+				'}';
+	}
 }
